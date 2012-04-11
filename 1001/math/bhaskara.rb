d9a96c4809039ca6ae1b24a1994c040a4944ccf5
Pedro Souza <pedrosouza@Pedro-Souzas-MacBook.local>
2012-04-11 10:29:13 -0300

    Adding bubble sort algorithm

---
+++ 1001/math/bhaskara.rb
@@ -0,0 +1,36 @@
+=begin
+
+Bhaskara
+Autor:
+    Bhaskara Akaria [1]
+Colaborador:
+    Pedro Henrique Pereira de Souza
+Tipo:
+    math
+Descrição:
+    Calcula as raízes de uma equação de segundo grau
+Complexidade:  
+    O(1)
+DIficuldade:
+    fácil
+Referências:
+    [1] http://pt.wikipedia.org/wiki/Bhaskara_Akaria
+    
+=end
+
+def bhaskara(a, b, c)
+	delta = b ** 2 - 4 * a * c
+	
+	if delta < 0
+		return nil
+	else
+		raizes = []
+		r1 = (-b + Math.sqrt(delta)) / (2*a)
+		raizes << r1
+		r2 = (-b - Math.sqrt(delta)) / (2*a)
+		raizes << r2
+	end
+end
+
+puts bhaskara(1, -1, -2)
+
---
+++ 1001/sort/bubblesort.rb
@@ -0,0 +1,31 @@
+=begin
+
+Bubblesort
+Autor:
+    ?
+Tipo:
+    Sorting
+Descrição:
+    Varre o vetor comparando cada um dos pares de números
+    possíveis e trocando suas posições no vetor se necessário
+Complexidade:  
+    O(n²)
+Dificuldade:
+    facil
+Referências:
+    http://en.wikipedia.org/wiki/Bubble_sort
+
+=end
+
+def bubblesort(vetor)
+	for i in 0..vetor.length - 1
+		for j in 0..vetor.length - 1
+			if vetor[i] < vetor[j]
+				vetor[i], vetor[j] = vetor[j], vetor[i]
+			end
+		end
+	end
+	return vetor
+end
+
+puts bubblesort([6, -7, 33, 12, 9, 19, 2])
